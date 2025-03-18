ff4c9640c2d0c659d0414ca0011f971322f9e17e
ajbzuo <abalasubramaniam@zuora.com>
2025-03-18 16:33:45 +0000

    Push

--- server/index.ts
+++ server/index.ts
@@ -53,8 +53,8 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  const port = 5000;
-  server.listen(port, "127.0.0.1", () => {
+  const port = process.env.PORT || 5000;
+  server.listen(port, "0.0.0.0", () => {
     log(`serving on port ${port}`);
   });
 })();
